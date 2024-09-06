ad6f7da2b11f31c05fc21efc31da9bd6ea05f083
Andrea Cervesato <andrea.cervesato@suse.com>
2024-09-06 16:34:30 +0200

    Format style terminal plugin
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,15 +1,15 @@
 return {
-    {
-        'akinsho/toggleterm.nvim',
-        version = "*",
-        config = function()
-            require("toggleterm").setup {
-                direction = "float",
-                open_mapping = [[<A-3>]],
-                shade_filetypes = {},
-                shade_terminals = true,
-                shade_factor = 1,
-            }
-        end,
-    },
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    config = function()
+      require('toggleterm').setup {
+        direction = 'float',
+        open_mapping = [[<A-3>]],
+        shade_filetypes = {},
+        shade_terminals = true,
+        shade_factor = 1,
+      }
+    end,
+  },
 }
