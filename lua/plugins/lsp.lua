55f4109f90a24b663c3e709c72bf0f91f551e1bb
Andrea Cervesato <andrea.cervesato@suse.com>
2025-04-06 08:08:07 +0200

    Disable 'vim' global undefined error

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -148,6 +148,7 @@ return {
         filetypes = { 'lua' },
         settings = {
           Lua = {
+            diagnostics = { globals = { 'vim' } },
             completion = {
               callSnippet = 'Replace',
             },
