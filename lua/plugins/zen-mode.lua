98d923e791a6494a8684c6fa57f56f5b83795606
Andrea Cervesato <andrea.cervesato@suse.com>
2025-08-11 12:41:16 +0200

    Remove zen-mode

--- lua/plugins/zen-mode.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'folke/zen-mode.nvim',
-  window = {
-    width = 120,
-  },
-  config = function()
-    vim.keymap.set('n', '<leader>z', ':ZenMode<CR>', { desc = 'Enter in Zen mode' })
-  end,
-}
