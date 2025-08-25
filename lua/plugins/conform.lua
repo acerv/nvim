f815895349ce692a4f781999b809cd75238a168a
Andrea Cervesato <andrea.cervesato@suse.com>
2025-08-25 13:28:21 +0200

    Add FormatEnable/FormatDisable commands
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,6 +4,9 @@ return {
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
         local disable_filetypes = {
           c = true,
           h = true,
@@ -30,5 +33,26 @@ return {
         json = { 'jd' },
       },
     },
+    config = function(_, opts)
+      vim.api.nvim_create_user_command('FormatDisable', function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = 'Disable autoformat-on-save',
+        bang = true,
+      })
+      vim.api.nvim_create_user_command('FormatEnable', function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = 'Re-enable autoformat-on-save',
+      })
+
+      require('conform').setup(opts)
+    end,
   },
 }
