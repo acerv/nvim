251a96f2c15f9e4e1cedf72746902d65d1d0f249
Andrea Cervesato <andrea.cervesato@mailbox.org>
2025-08-27 09:21:15 +0200

    dashboard: show loaded plugins and startup time

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -27,6 +27,20 @@ return {
 
       vim.cmd [[autocmd User AlphaReady echo 'ready']]
 
+      -- Footer setup
+      local function setup_footer()
+        vim.api.nvim_create_autocmd('User', {
+          pattern = 'LazyVimStarted',
+          callback = function()
+            local lazy = require('lazy').stats()
+            dashboard.section.footer.val = string.format('⚡ Loaded %d/%d plugins in %.2fms', lazy.loaded, lazy.count, lazy.startuptime)
+            pcall(vim.cmd.AlphaRedraw)
+          end,
+        })
+      end
+
+      setup_footer()
+
       alpha.setup(dashboard.config)
     end,
   },
