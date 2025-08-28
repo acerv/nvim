1823095ecbc2ad81360ab922d4048042802fa0b9
Andrea Cervesato <andrea.cervesato@suse.com>
2025-08-28 08:52:45 +0200

    neo-tree: follow open file

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,6 +8,21 @@ return {
       'nvim-tree/nvim-web-devicons',
     },
     config = function()
+      require('neo-tree').setup {
+        filesystem = {
+          filtered_items = {
+            visible = true,
+            show_hidden_count = true,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+          },
+          follow_current_file = {
+            enabled = true,
+            leave_dirs_open = false,
+          },
+        },
+        buffers = { follow_current_file = { enable = true } },
+      }
       vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle<CR>', { desc = 'File Explorer' })
     end,
   },
@@ -24,6 +39,7 @@ return {
   {
     's1n7ax/nvim-window-picker',
     version = '2.*',
+    event = 'VeryLazy',
     config = function()
       require('window-picker').setup {
         filter_rules = {
