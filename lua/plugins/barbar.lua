18fdf003b63764a852039f39e3f35ec1867c0927
Andrea Cervesato <andrea.cervesato@suse.com>
2025-08-28 13:53:34 +0200

    barbar: fix typo

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -10,7 +10,7 @@ return {
 
       vim.keymap.set('n', '<leader>bb', '<cmd>BufferPrevious<CR>', { desc = 'Previous buffer' })
       vim.keymap.set('n', '<leader>bn', '<cmd>BufferNext<CR>', { desc = 'Next buffer' })
-      vim.keymap.set('n', '<leader>ba', '<cmd>BufferCloseAllButCurrent<CR>', { desc = 'Close all buffers but the current one' })
+      vim.keymap.set('n', '<leader>ba', '<cmd>BufferCloseAllButCurrent<CR>', { desc = 'Close other buffers' })
       vim.keymap.set('n', '<leader>bc', '<cmd>BufferClose<CR>', { desc = 'Close buffer' })
       vim.keymap.set('n', '<leader>bA', '<cmd>BufferCloseAllButPinned<CR>', { desc = 'Close all buffers' })
     end,
