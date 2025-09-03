4fe340c59dd4ebeec6e556562c24124b605e5e3f
Andrea Cervesato <andrea.cervesato@suse.com>
2025-09-03 14:32:13 +0200

    indent: use guess-indent
    Remove lazy-plugins.lua and initialize Lazy via lazy-bootstrap.lua,
    then use guess-indent instead of the old vim plugin.

--- lua/lazy-bootstrap.lua
+++ lua/lazy-bootstrap.lua
@@ -8,3 +8,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  { import = 'plugins' },
+})
