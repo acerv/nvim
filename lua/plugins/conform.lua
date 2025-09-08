f35770f5959b3ae5a66a0ff99ba36907b4dc0833
Andrea Cervesato <andrea.cervesato@suse.com>
2025-09-08 14:01:26 +0200

    conform: auto-format toggle binding

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -35,23 +35,19 @@ return {
       },
     },
     config = function(_, opts)
-      vim.api.nvim_create_user_command('FormatDisable', function(args)
+      vim.api.nvim_create_user_command('ToggleFormat', function(args)
         if args.bang then
           -- FormatDisable! will disable formatting just for this buffer
-          vim.b.disable_autoformat = true
+          vim.b.disable_autoformat = not vim.b.disable_autoformat
         else
-          vim.g.disable_autoformat = true
+          vim.g.disable_autoformat = not vim.g.disable_autoformat
         end
       end, {
         desc = 'Disable autoformat-on-save',
         bang = true,
       })
-      vim.api.nvim_create_user_command('FormatEnable', function()
-        vim.b.disable_autoformat = false
-        vim.g.disable_autoformat = false
-      end, {
-        desc = 'Re-enable autoformat-on-save',
-      })
+
+      vim.keymap.set('n', '<leader>uf', '<cmd>ToggleFormat<CR>', { desc = 'Toggle Code Formatting' })
 
       require('conform').setup(opts)
     end,
