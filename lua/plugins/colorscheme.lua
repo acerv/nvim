70fbb3745629396997b67c5b4af3ab531998286b
Andrea Cervesato <andrea.cervesato@suse.com>
2025-09-09 09:48:27 +0200

    colorscheme: set python docstring as comment

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,6 +5,7 @@ return {
     priority = 1000,
     config = function()
       vim.cmd 'colorscheme tokyonight-night'
+      vim.cmd 'hi link @string.documentation.python Comment'
     end,
   },
   {
