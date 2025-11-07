4fc571c953bf756b8d57bf8a20021a88ccab7d7c
Andrea Cervesato <andrea.cervesato@suse.com>
2025-11-07 09:46:50 +0100

    mason: update tools automatically
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -7,6 +7,7 @@ return {
     config = function()
       require('mason').setup {}
       require('mason-tool-installer').setup {
+        auto_update = true,
         ensure_installed = {
           'jq',
           'ruff',
